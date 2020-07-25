0cc374bf1c64183743e35c8b8256ed2be4cb2de8
eaczechova <ewa.czech@hotmail.com>
2020-07-25 18:46:48 +0200

    add solution for stack

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,35 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if len(self.storage) > 0:
+            return self.storage.pop()
+        return None
+
+# from singly_linked_list import LinkedList
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.add_to_head(value)
+#         self.size += 1
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         self.size -= 1
+#         return self.storage.remove_head()
